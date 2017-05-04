63094b3913fae534fdb70bbcc73e877f63c530a5
Dominic Marra <dominicmarra321@gmail.com>
2017-05-04 12:12:31 -0700

    Updated login screen

--- public/script/signin.js
+++ public/script/signin.js
@@ -1,22 +1,15 @@
+var uiConfig = {
+signInSuccessUrl: 'home.html',
+signInOptions: [
+  // Leave the lines as is for the providers you want to offer your users.
+  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  firebase.auth.EmailAuthProvider.PROVIDER_ID
+],
+// Terms of service url.
+tosUrl: '<your-tos-url>'
+};
 
-
-function signIn() {
-  
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-    });
-}
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
