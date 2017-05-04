6ac3052419942faafb0ad682b3449cb544a22100
Dominic Marra <dominicmarra321@gmail.com>
2017-05-03 23:13:55 -0700

    Set up firebase, changed some UI, added google sign in

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php include("include/header.php"); ?>
-<div>Testing</div>
-<?php include("include/footer.php"); ?>
---
+++ public/script/firebaseStart.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBoXua9_BMns2r320oqEIpvLmrKpmkwX6o",
+    authDomain: "wastebook-2e70b.firebaseapp.com",
+    databaseURL: "https://wastebook-2e70b.firebaseio.com",
+    projectId: "wastebook-2e70b",
+    storageBucket: "wastebook-2e70b.appspot.com",
+    messagingSenderId: "296254473379"
+};
+firebase.initializeApp(config);
